c63a0e32621e6b1e80896e1a8219e1960e0d349a
1liale <1alexli2002@gmail.com>
2025-01-07 18:37:17 -0500

    added firebase cloud storage for media upload support

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,13 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-firebase-storage",
+      providerOptions: {
+        serviceAccount: JSON.parse(env('FIREBASE_SERVICE_ACCOUNT', '{}')),
+        bucket: env('STORAGE_BUCKET_URL', "debatemodai.firebasestorage.app"),
+        sortInStorage: true,
+        debug: false,
+      },
+    },
+  },
+});
